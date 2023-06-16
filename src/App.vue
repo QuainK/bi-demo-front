4b2c4554a90c65875ed0f16c49d80ac69445ecb9
Qian Kun <1324575148@qq.com>
2023-06-16 14:17:18 +0800

    feat: 上传文件，并将表格解析成JSON

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <DataImport />
+  <div class="container">
+    <DataImport />
+  </div>
 </template>
 
 <script setup lang="ts">
 import DataImport from '@/views/data-import.vue'
 </script>
 
-<style scoped lang="sass">
+<style scoped lang="scss">
+.container {
+  padding: 10px;
+}
 </style>
--- src/views/data-import.vue
+++ src/views/data-import.vue
@@ -1,9 +1,56 @@
 <template>
-
+  <div class="box">
+    <div class="upload">
+      <el-upload
+        ref="upload"
+        action="#"
+        drag
+        :limit="1"
+        :auto-upload="false"
+        :on-change="handleChange"
+        :on-exceed="handleExceed"
+      >上传文件
+      </el-upload>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { genFileId, UploadFile, UploadProps, UploadRawFile } from 'element-plus'
+import * as x from 'xlsx'
+import { ref } from 'vue'
+
+const upload = ref()
+
+const handleExceed: UploadProps['onExceed'] = (files) => {
+  upload.value!.clearFiles()
+  const file = files[0] as UploadRawFile
+  file.uid = genFileId()
+  upload.value!.handleStart(file)
+}
+
+const handleChange = (uploadFile: UploadFile) => {
+  const file = uploadFile.raw as Blob
+
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    const buffer = e.target!.result
+    const workbook = x.read(buffer, { type: 'array' })
+    const sheetName = workbook.Sheets[workbook.SheetNames[0]]
+    const json = x.utils.sheet_to_json(sheetName)
+    console.log('json', json)
+  }
+  reader.readAsArrayBuffer(file)
+}
 </script>
 
-<style scoped lang="sass">
+<style scoped lang="scss">
+.box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.upload {
+  width: 300px;
+}
 </style>
