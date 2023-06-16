508a2f41765dbc5c76a26156cc07759e58e4bebc
Qian Kun <1324575148@qq.com>
2023-06-16 16:46:33 +0800

    feat: 添加数据表格展示组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
     <DataImport />
+    <DataShow />
   </div>
 </template>
 
 <script setup lang="ts">
 import DataImport from '@/views/data-import.vue'
+import DataShow from '@/views/data-show.vue'
 </script>
 
 <style scoped lang="scss">
 .container {
-  padding: 10px;
+  padding: 50px;
 }
 </style>
---
+++ src/views/data-show.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-table
+    :data="data"
+    stripe
+    border
+  >
+    <el-table-column
+      v-for="(columnProp,index) in columnPropList "
+      :key="index"
+      :prop="columnProp"
+      :label="columnProp"
+    />
+  </el-table>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const data = ref([
+  { a: 1, b: 2, c: 3, '中文表头': '哈哈哈' },
+  { a: 1, b: 2, c: 3, '中文表头': '哈哈哈' },
+  { a: 1, b: 2, c: 3, '中文表头': '哈哈哈' },
+  { a: 1, b: 2, c: 3, '中文表头': '哈哈哈' },
+  { a: 1, b: 2, c: 3, '中文表头': '哈哈哈' },
+  { a: 1, b: 2, c: 3, '中文表头': '哈哈哈' },
+])
+
+const columnPropList = ref<string[]>([])
+
+if (data.value.length) {
+  columnPropList.value = [...Object.keys(data.value[0])]
+}
+</script>
+
+<style scoped lang="scss">
+</style>
