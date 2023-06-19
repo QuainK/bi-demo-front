9b1419fa3ba9e086a10cf7804ce3ec1cbb781e3e
Qian Kun <1324575148@qq.com>
2023-06-19 15:44:12 +0800

    feat: 表格筛选

--- src/views/data-source/Index.vue
+++ src/views/data-source/Index.vue
@@ -12,6 +12,7 @@
         <template v-if="originalData.length">
           <el-button @click="onClickDeleteRedundant">删除重复行</el-button>
           <el-button @click="onClickSort">排序</el-button>
+          <el-button @click="onClickFilter">筛选</el-button>
         </template>
       </div>
 
@@ -25,7 +26,8 @@
 
         <!--表单-->
         <div class="data-menu-detail-form">
-          <template v-if="toolVisible.sort">
+          <!--排序-->
+          <div v-show="toolVisible.sort">
             <el-form inline>
               <el-form-item>
                 <span class="form-item-label">选择字段：</span>
@@ -51,7 +53,41 @@
                 </el-select>
               </el-form-item>
             </el-form>
-          </template>
+          </div>
+
+          <!--筛选-->
+          <div v-show="toolVisible.filter">
+            <el-form
+              v-for="(filter, index) in filterList"
+              :key="index"
+              label-position="top"
+              inline
+            >
+              <el-form-item :label="index === 0 ? '字段' : ''">
+                <el-select v-model="filter.column" placeholder="请选择字段">
+                  <el-option
+                    v-for="column in columnPropList"
+                    :key="column"
+                    :value="column"
+                    :label="column"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item :label="index === 0 ? '运算符' : ''">
+                <el-select v-model="filter.operator" placeholder="请选择运算符">
+                  <el-option
+                    v-for="operator in Object.values(operatorList)"
+                    :key="operator.name"
+                    :value="operator.name"
+                    :label="operator.text"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item :label="index === 0 ? '值' : ''">
+                <el-input v-model.trim="filter.content" placeholder="请输入值" />
+              </el-form-item>
+            </el-form>
+          </div>
         </div>
       </div>
 
@@ -157,6 +193,69 @@ const handleDataChange = (val: any) => {
   local.set('dataHeader', columnPropList.value)
 }
 
+// ---------- 筛选 开始 ----------
+
+// 点击筛选按钮
+const onClickFilter = () => {
+  closeAllTool()
+  toolBoxVisible.value = true
+  toolVisible.filter = true
+}
+
+// 过滤条件列表
+const filterList = ref([{
+  // 字段名
+  column: 'ID',
+  // 运算符
+  operator: 'include',
+  // 值，为什么不用value，防止和ref的.value混淆
+  content: '71',
+}])
+// 运算符常量列表
+const operatorList = ref({
+  include: { name: 'include', text: '包含' },
+  exclude: { name: 'exclude', text: '不包含' },
+  prefix: { name: 'prefix', text: '开头是' },
+  suffix: { name: 'suffix', text: '结尾是' },
+})
+// 点击确定按钮
+const onClickFilterConfirm = () => {
+  // 表格数据的筛选后结果
+  let list = JSON.parse(JSON.stringify(originalData))
+
+  // 遍历筛选条件列表
+  filterList.value.forEach((condition) => {
+    list = list.filter((item: any) => {
+      // 目标字符串
+      const target = item[condition.column].toString()
+      // 匹配子串
+      const pattern = condition.content
+
+      // 判断分支
+      if (condition.operator === 'include' || condition.operator === 'exclude') {
+        // 包含和不包含
+        const result = target.includes(pattern)
+        return condition.operator === 'include' ? result : !result
+      } else if (condition.operator === 'prefix') {
+        // 开头是
+        return target.startsWith(pattern)
+      } else if (condition.operator === 'suffix') {
+        // 结尾是
+        return target.endsWith(pattern)
+      } else {
+        // 啥都不是
+        return false
+      }
+    })
+  })
+
+  // 更新表格
+  data.length = 0
+  Object.assign(data, JSON.parse(JSON.stringify(list)))
+}
+
+// ---------- 筛选 结束 ----------
+
 const header = local.get('dataHeader') ?? []
 const body = local.get('dataBody') ?? []
 
@@ -170,6 +269,7 @@ const toolBoxVisible = ref(false)
 const toolVisible = reactive<{ [prop: string]: boolean }>({
   deleteRedundant: false,
   sort: false,
+  filter: false,
 })
 const closeAllTool = () => {
   Object.keys(toolVisible).forEach((key) => {
@@ -193,12 +293,14 @@ const onClickToolConfirm = () => {
     const arr = JSON.parse(JSON.stringify(temp))
     data.length = 0
     Object.assign(data, arr)
+  } else if (toolVisible.filter) {
+    onClickFilterConfirm()
   }
 }
 
 const onClickSort = () => {
-  toolBoxVisible.value = true
   closeAllTool()
+  toolBoxVisible.value = true
   toolVisible.sort = true
 }
 const sortColumn = ref('')
